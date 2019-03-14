a09e1f395752c7a8375619637b06c666d91a1c98
“5sWind” <fiveswind@gmail.com>
2019-03-14 23:57:10 +0800

    Update p2p config and shell scripts.

--- samples/simple-network/config.sh
+++ samples/simple-network/config.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Copyright 2019 IBM Corp.
 
-# Licensed under the Apache License, Version 2.0 (the "License"); 
-# you may not use this file except in compliance with the License. 
-# You may obtain a copy of the License at 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 
-#   http://www.apache.org/licenses/LICENSE-2.0 
+#   http://www.apache.org/licenses/LICENSE-2.0
 
-# Unless required by applicable law or agreed to in writing, software 
-# distributed under the License is distributed on an "AS IS" BASIS, 
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-# See the License for the specific language governing permissions and 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
 # limitations under the License.
 
 # The file will ensure that each nodes (whether servers or peers) have different peer id.
@@ -23,56 +23,85 @@ export IPFS_CONFIG=${IPFS_PATH}/config
 export SWARM_KEY_FILE=${IPFS_PATH}/swarm.key
 
 # Print the help message.
-function printHelper () {
-    echo "Usage: "
-    echo "  config.sh init   - initialize IPFS config if not already initialized."
-    echo "  config.sh daemon - run IPFS daemon process for target network."
-    echo "Flags: "
-    echo "  -p <profile> - the IPFS profile for initialization."
-    echo "  --routing <routing> - overrides the routing option (defaults to default)."
+function printHelper() {
+	echo "Usage: "
+	echo "  config.sh init - initialize IPFS config if not already initialized."
+	echo "  config.sh daemon - run IPFS daemon process for target network."
+	echo "Flags: "
+    echo "  -p <profile> - the IPFS profile for initialization (defaults to default-networking)."
+    echo "  -r <routing> - routing option for IPFS node (defaults to default)."
+	echo "	-m <migrate> - option for auto repo migration (defaults to false)."
 }
 
 # Check whether ipfs configuration file already exists.
-function init () {
-    if [ ! -e "$IPFS_CONFIG" ]; then
-        echo "---- No IPFS configuration file found, ${MESSAGE}... ----"
-        ipfs init
-    fi
+function init() {
+	if [ ! -e "$IPFS_CONFIG" ]; then
+		echo "---- No IPFS configuration file found, ${MESSAGE}... ----"
+		ipfs init --profile=$PROFILE
+	fi
 }
 
 # Running IPFS daemon process.
-function daemon () {
-    if [ ! -e "$SWARM_KEY_FILE" ]; then
-        echo "---- Swarm key file not found, ${MESSAGE} a default network. ----"
-    else
-        echo "---- ${MESSAGE} a private network with a swarm key file. ----"
-        export LIBP2P_FORCE_PNET=$PNET
-    fi
-    ipfs daemon
+function daemon() {
+	if [ ! -e "$SWARM_KEY_FILE" ]; then
+		echo "---- Swarm key file not found, ${MESSAGE} a default network. ----"
+	else
+		echo "---- ${MESSAGE} a private network with a swarm key file. ----"
+		set -x
+		LIBP2P_FORCE_PNET=$PNET
+		set +x
+	fi
+	ipfs daemon --routing=$ROUTING --migrate=$MIGRATE
 }
 
 # Set private network
 PNET=1
+# Set config profile
+PROFILE=default-networking
+# Set routing option
+ROUTING=default
+# Set repo migration
+MIGRATE=false
 
 # The arg of the command
-COMMAND=$1;shift
+COMMAND=$1
+shift
+
+# Options for running command
+while getopts "h?p:r:m" opt; do
+	case "$opt" in
+	h | \?)
+		printHelper
+		exit 0
+		;;
+	p)
+		PROFILE=$OPTARG
+		;;
+	r)
+		ROUTING=$OPTARG
+		;;
+	m)
+		MIGRATE=true
+		;;
+	esac
+done
 
 # Command interface for message
 if [ "$COMMAND" == "init" ]; then
-    MESSAGE="Initializing"
+	MESSAGE="Initializing"
 elif [ "$COMMAND" == "daemon" ]; then
-    MESSAGE="Starting"
+	MESSAGE="Starting"
 else
-    printHelper
-    exit 1
+	printHelper
+	exit 1
 fi
 
 # Command interface for execution
 if [ "${COMMAND}" == "init" ]; then
-    init
+	init
 elif [ "${COMMAND}" == "daemon" ]; then
-    daemon
+	daemon
 else
-    printHelper
-    exit 1
+	printHelper
+	exit 1
 fi
--- samples/simple-network/pnet.sh
+++ samples/simple-network/pnet.sh
@@ -1,139 +1,166 @@
 #!/bin/bash
 # Copyright 2019 IBM Corp.
 
-# Licensed under the Apache License, Version 2.0 (the "License"); 
-# you may not use this file except in compliance with the License. 
-# You may obtain a copy of the License at 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 
-#   http://www.apache.org/licenses/LICENSE-2.0 
+#   http://www.apache.org/licenses/LICENSE-2.0
 
-# Unless required by applicable law or agreed to in writing, software 
-# distributed under the License is distributed on an "AS IS" BASIS, 
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-# See the License for the specific language governing permissions and 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
 # limitations under the License.
 
 # Setting environment variable
-export PATH=${PWD}:$PATH
+export PATH=${PWD}/../../.bin:${PWD}:$PATH
 export IPFS_PATH=${PWD}/../../.build
 
 # Print the help message.
-function printHelper () {
-    echo "Usage: "
-    echo "  pnet.sh <command> <subcommand>"
-    echo "      <command> - one of 'start', 'stop' or 'restart'."
-    echo "          - 'start' - start and up the network with docker-compose up."
-    echo "          - 'stop' - stop and clear the network with docker-compose down."
-    echo "          - 'restart' - restart the network."
-    echo "      <subcommand> - networks type, <subcommand=p2p|p2s|p2sp>."
-    echo "Flags: "
-    echo "  -n <network> - print all available network."
-    echo "  -i <imagetag> - the tag for the private network launch (defaults to latest)."
-    echo "  -f <composefile> - docker-compose file to be selected (defaults to docker-compose.yml)."
+function printHelper() {
+	echo "Usage: "
+	echo "  pnet.sh <command> <subcommand>"
+	echo "      <command> - one of 'up', 'down' or 'restart'."
+	echo "          - 'up' - start and up the network with docker-compose up."
+	echo "          - 'down' - stop and clear the network with docker-compose down."
+	echo "          - 'restart' - restart the network."
+	echo "      <subcommand> - network type, <subcommand=p2p|p2s|p2sp>."
+	echo "Flags: "
+	echo "  -n <network> - print all available network."
+	echo "  -i <imagetag> - the tag for the private network launch (defaults to latest)."
+	echo "  -f <composefile> - docker-compose file to be selected (defaults to docker-compose.yml)."
 }
 
 # Print all network.
-function printNetwork () {
-    echo "Usage: "
-    echo "  pnet.sh <command> <subcommand>"
-    echo "      <command> - <command=start|stop|restart> corresponding network based on user choice."
-    echo "      <subcommand> - one of 'p2p', 'p2s', or 'p2sp'."
-    echo "          - 'p2p' - a peer-to-peer based, private network."
-    echo "          - 'p2s' - a peer-to-server based, private network."
-    echo "          - 'p2sp' - a peer to server and to peer based, private network."
-    echo
-    echo "Typically, one can bring up the network through subcommand e.g.:"
-    echo
-    echo "      ./pnet.sh start p2p"
-    echo
+function printNetwork() {
+	echo "Usage: "
+	echo "  pnet.sh <command> <subcommand>"
+	echo "      <command> - <command=up|down|restart> corresponding network based on user choice."
+	echo "      <subcommand> - one of 'p2p', 'p2s', or 'p2sp'."
+	echo "          - 'p2p' - a peer-to-peer based, private network."
+	echo "          - 'p2s' - a peer-to-server based, private network."
+	echo "          - 'p2sp' - a peer to server and to peer based, private network."
+	echo
+	echo "Typically, one can bring up the network through subcommand e.g.:"
+	echo
+	echo "      ./pnet.sh up p2p"
+	echo
 }
 
 # Generate swarm key
-function generateKey () {
-    which swarmkeygen
-    if [ "$?" -ne 0 ]; then
-        echo "swarmkeygen tool not found, exit."
-        exit 1
-    fi
-    echo "---- Generate swarm.key file using swarmkeygen tool. ----"
-    set -x
-    swarmkeygen generate > $IPFS_PATH/swarm.key
-    res=$?
-    set +x
-    if [ $res -ne 0 ]; then
-        echo "Failed to generate swarm.key file, exit."
-        exit 1
-    fi
+function generateKey() {
+	which swarmkeygen
+	if [ "$?" -ne 0 ]; then
+		echo "swarmkeygen tool not found, exit."
+		exit 1
+	fi
+	echo "---- Generate swarm.key file using swarmkeygen tool. ----"
+	set -x
+	swarmkeygen generate >$IPFS_PATH/swarm.key
+	res=$?
+	set +x
+	if [ $res -ne 0 ]; then
+		echo "Failed to generate swarm.key file, exit."
+		exit 1
+	fi
 }
 
 # Create containers environment
-function createContainers () {
-    echo "---- Creat containers for running IPFS. ----"
-    for PEER in peer0.example.com peer1.example.com; do
-        IMAGE_TAG=$IMAGETAG docker-compose -f $COMPOSE_FILE_P2P up --no-start $PEER
-    done
+function createContainers() {
+	echo "---- Creat containers for running IPFS. ----"
+	for PEER in peer0.example.com peer1.example.com; do
+		IMAGE_TAG=$IMAGETAG docker-compose -f $COMPOSE_FILE_P2P up --no-start $PEER
+	done
 }
 
 # Copy swarm key file into container
-function copySwarmKey () {
-    echo "---- Copy swarm key file into the container file system. ----"
-    for PEER in peer0.example.com peer1.example.com; do
-        set -x
-        docker cp -a $IPFS_PATH/swarm.key $PEER:/var/ipfsfb
-        set +x
-    done
+function copySwarmKey() {
+	echo "---- Copy swarm key file into the container file system. ----"
+	for PEER in peer0.example.com peer1.example.com; do
+		set -x
+		docker cp -a $IPFS_PATH/swarm.key $PEER:/var/ipfsfb
+		set +x
+	done
 }
 
 # Start containers
-function startContainers () {
-    echo "---- Start containers using secret swarm key. ----"
-    for PEER in peer0.example.com peer1.example.com; do
-        IMAGE_TAG=$IMAGETAG docker-compose -f $COMPOSE_FILE_P2P start $PEER
-    done
-    echo "---- Sleeping 10s to allow network complete booting. ----"
-    sleep 10
+function startContainers() {
+	echo "---- Start containers using secret swarm key. ----"
+	for PEER in peer0.example.com peer1.example.com; do
+		IMAGE_TAG=$IMAGETAG docker-compose -f $COMPOSE_FILE_P2P start $PEER
+	done
+	echo "---- Sleeping 12s to allow network complete booting. ----"
+	sleep 12
 }
 
 # Set and switch to private network
-function privateNetUp () {
-    echo "---- Configure the private network. ----"
-    for PEER in peer0.example.com peer1.example.com; do
-        docker exec $PEER ipfs bootstrap rm --all
-    done
-    PEER_ADDR=$(docker exec peer0.example.com ipfs id -f='<addrs>' | tail -n 1)
-    docker exec peer1.example.com ipfs bootstrap add $PEER_ADDR
-    PEER_ADDR=$(docker exec peer1.example.com ipfs id -f='<addrs>' | tail -n 1)
-    docker exec peer0.example.com ipfs bootstrap add $PEER_ADDR
+function switchPrivateNet() {
+	echo "---- Configure the private network. ----"
+	for PEER in peer0.example.com peer1.example.com; do
+		docker exec $PEER ipfs bootstrap rm --all
+	done
+	PEER_ADDR=$(docker exec peer0.example.com ipfs id -f='<addrs>' | tail -n 1)
+	docker exec peer1.example.com ipfs bootstrap add $PEER_ADDR
+	PEER_ADDR=$(docker exec peer1.example.com ipfs id -f='<addrs>' | tail -n 1)
+	docker exec peer0.example.com ipfs bootstrap add $PEER_ADDR
 }
 
-# Start and up a peer to peer based network
-function startP2p () {
-    if [ -d "$IPFS_PATH" ]; then
-        generateKey
-        createContainers
-        copySwarmKey
-        startContainers
-        privateNetUp
-    fi
-    IMAGE_TAG=$IMAGETAG docker-compose -f $COMPOSE_FILE_P2P up -d 2>&1
-    if [ $? -ne 0 ]; then
-        echo "ERROR!!! could not start p2p network, exit."
-        exit 1
-    fi
+# Restart containers for the private network.
+function restartContainers() {
+	echo "---- Restart containers for the configured private network. ----"
+	for PEER in peer0.example.com peer1.example.com; do
+		IMAGE_TAG=$IMAGETAG docker-compose -f $COMPOSE_FILE_P2P restart $PEER
+	done
 }
 
-# Stop and clear peer to peer based network
-function stopP2p () {
-    docker-compose -f $COMPOSE_FILE_P2P down --volumes --remove-orphans
+# Start and up a peer to peer based private network
+function p2pUp() {
+	if [ -d "$IPFS_PATH" ]; then
+		generateKey
+		createContainers
+		copySwarmKey
+		startContainers
+		switchPrivateNet
+		restartContainers
+	fi
+	IMAGE_TAG=$IMAGETAG docker-compose -f $COMPOSE_FILE_P2P up -d --no-deps cli 2>&1
+	if [ $? -ne 0 ]; then
+		echo "ERROR!!! could not start p2p network, exit."
+		exit 1
+	fi
 }
 
-# Start a peer to server based network
-#function startP2s () {
+# Stop and clear peer to peer based private network
+function p2pDown() {
+	# Bring down the private network, and remove volumes.
+	docker-compose -f $COMPOSE_FILE_P2P down --volumes --remove-orphans
+	if [ "$COMMAND" != "restart" ]; then
+		docker run -v $PWD:/var/ipfsfb --rm ipfsfb/ipfs-tools:$IMAGETAG rm -rf /var/ipfsfb/peer /var/ipfsfb/data /var/ipfsfb/staging
+		# Remove local ipfs config.
+		rm -rf .ipfs/data .ipfs/staging
+		# Remove unwanted key file generated by swarmkeygen tool.
+		rm -f $IPFS_PATH/*.key
+	fi
+}
+
+# Start and up a peer to server based private network
+#function p2sUp () {
 
 #}
 
-# Start a peer to server and to peer based network
-#function startP2sp () {
+# Stop and clear peer to server based private network
+#function p2sDown () {
+
+#}
+
+# Start and up a peer to server and to peer based private network
+#function p2spUp () {
+
+#}
+
+# Stop and clear peer to server and to peer based private network
+#function p2spDown () {
 
 #}
 
@@ -148,51 +175,67 @@ IMAGETAG=latest
 
 # Options for running command
 while getopts "h?n?i:f:" opt; do
-    case "$opt" in
-        h | \?)
-            printHelper
-            exit 0
-            ;;
-        n)
-            printNetwork
-            exit 0
-            ;;
-        i)
-            IMAGETAG=$OPTARG
-            ;;
-        f)
-            COMPOSE_FILE=$OPTARG
-            ;;
-    esac
+	case "$opt" in
+	h | \?)
+		printHelper
+		exit 0
+		;;
+	n)
+		printNetwork
+		exit 0
+		;;
+	i)
+		IMAGETAG=$OPTARG
+		;;
+	f)
+		COMPOSE_FILE=$OPTARG
+		;;
+	esac
 done
 
 # The arg of the command
-COMMAND=$1;SUBCOMMAND=$2;shift
+COMMAND=$1
+SUBCOMMAND=$2
+shift
 
 # Command interface for execution
-if [ "${COMMAND}" == "start" ]; then
-    if [ "${SUBCOMMAND}" == "p2p" ]; then
-        startP2p
-    elif [ "${SUBCOMMAND}" == "p2s" ]; then
-        startP2s
-    elif [ "${SUBCOMMAND}" == "p2sp" ]; then
-        startP2sp
-    else
-        printNetwork
-        exit 1
-    fi
-elif [ "${COMMAND}" == "stop" ]; then
-    if [ "${SUBCOMMAND}" == "p2p" ]; then
-        stopP2p
-    elif [ "${SUBCOMMAND}" == "p2s" ]; then
-        stopP2s
-    elif [ "${SUBCOMMAND}" == "p2sp" ]; then
-        stopP2sp
-    else
-        printNetwork
-        exit 1
-    fi
+if [ "${COMMAND}" == "up" ]; then
+	if [ "${SUBCOMMAND}" == "p2p" ]; then
+		p2pUp
+	elif [ "${SUBCOMMAND}" == "p2s" ]; then
+		p2sUp
+	elif [ "${SUBCOMMAND}" == "p2sp" ]; then
+		p2spUp
+	else
+		printNetwork
+		exit 1
+	fi
+elif [ "${COMMAND}" == "down" ]; then
+	if [ "${SUBCOMMAND}" == "p2p" ]; then
+		p2pDown
+	elif [ "${SUBCOMMAND}" == "p2s" ]; then
+		p2sDown
+	elif [ "${SUBCOMMAND}" == "p2sp" ]; then
+		p2spDown
+	else
+		printNetwork
+		exit 1
+	fi
+elif [ "${COMMAND}" == "restart" ]; then
+	if [ "${SUBCOMMAND}" == "p2p" ]; then
+		p2pDown
+		p2pUp
+	elif [ "${SUBCOMMAND}" == "p2s" ]; then
+		p2sDown
+		p2sUp
+	elif [ "${SUBCOMMAND}" == "p2sp" ]; then
+		p2spDown
+		p2spUp
+	else
+		printNetwork
+		exit 1
+	fi
 else
-    printHelper
-    exit 1
+	printHelper
+	exit 1
 fi
